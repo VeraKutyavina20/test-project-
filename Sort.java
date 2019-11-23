bdd32621b2edde872854c11d72b7f27a758a2605
Vera <verakut9@gmail.com>
2019-11-23 18:10:26 +0300

    new

---
+++ Sort.java
@@ -0,0 +1,10 @@
+public static void insertionSort2(int[] m, int a, int b)
+{
+    int i, j, t;
+    for (i = a; i < b; i++)
+    {
+        t = m[i];
+        for (j = i - 1; j >= a - 1 && m[j] > t; j--) m[j + 1] = m[j];
+        m[j + 1] = t;
+    }
+}
