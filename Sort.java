7cbc660dd39393bb60865ba9918c1d6cb9a7b5e4
VeraKutyavina20 <57903183+VeraKutyavina20@users.noreply.github.com>
2019-12-01 18:17:44 +0300

    Update

--- Sort.java
+++ Sort.java
@@ -1,12 +1,14 @@
-public static void insertionSort2(int[] m, int a, int b)
-{
-    int i, j, t;
-    for (i = a; i < b; i++)
+public class Sort {
+    public static void insertionSort2(int[] m, int a, int b)
     {
-        t = m[i];
-        for (j = i - 1; j >= a - 1 && m[j] > t; j--) {
-            m[j + 1] = m[j];
-            m[j + 1] = t;
+        int i, j, t;
+        for (i = a; i < b; i++)
+        {
+            t = m[i];
+            for (j = i - 1; j >= a - 1 && m[j] > t; j--) {
+                m[j + 1] = m[j];
+                m[j + 1] = t;
+            }
         }
     }
 }
